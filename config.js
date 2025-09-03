f951b92323aacf212ed5b5e1f29b5c517de35c55
kkondegarsong <kkondegarsong@gmail.com>
2025-09-03 15:25:22 +0900

    :sparkles: feat: config 파일 추가.

---
+++ config.js
@@ -0,0 +1,18 @@
+const config = {
+    browser: {
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        locale: 'ko-KR',
+        timezoneId: 'Asia/Seoul'
+    },
+    storage: {
+        cookies: './browser-profiles/instgram-cookies.json',
+        downloads: './downloads',
+        browser: './browser-profiles'
+    },
+    url: {
+        login: 'https://www.instagram.com/accounts/login/',
+        baseUrl: 'https://www.instagram.com/'
+    }
+}
+
+module.exports = config;
